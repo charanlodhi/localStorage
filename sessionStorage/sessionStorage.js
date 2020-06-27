4f8bcd270fd0d87d347628555132d2c61a007d30
Charanlodhi <charansingh.lodhi82@gmail.com>
2020-06-27 09:56:31 +0530

    session storage

---
+++ sessionStorage/sessionStorage.js
@@ -0,0 +1,8 @@
+sessionStorage.setItem('keyName', 'sample data')
+sessionStorage.getItem('keyName')
+sessionStorage.removeItem('keyName')
+sessionStorage.setItem('keyName', 'sample data')
+
+undefined
+sessionStorage.getItem('keyName')
+"sample data"
